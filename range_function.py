3021916c85ec4eadd4011703b937e1a3d5bea75d
wctan <100176525+wctan@users.noreply.github.com>
2022-10-31 17:21:12 +0800

    Create range_function.py

---
+++ range_function.py
@@ -0,0 +1,15 @@
+print('The function range() with one parameter\n')
+
+for _range in range(8):
+  print(_range, end=' ')
+
+print('\nThe function range() with two parameter\n')
+for _range in range(8,20):
+  print(_range, end=' ')
+
+
+print('\nThe function range() with three parameter\n')
+for _range in range(8,20,2):
+  print(_range, end=' ')
+
+print('\n')
